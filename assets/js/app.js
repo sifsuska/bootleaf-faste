0d5b64d3379e83bb47d026fa422184dfb7e5dc83
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2017-05-03 07:17:41 +0700

    Add files via upload
    Enable categorize room with colour base type room.

--- assets/js/app.js
+++ assets/js/app.js
@@ -137,14 +137,14 @@ function syncSidebar() {
 
 /* Basemap Layers */
 var cartoLight = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
-  //maxZoom: 19,
+  maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
 });
 var usgsImagery = L.layerGroup([L.tileLayer("http://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}", {
-  //maxZoom: 15,
+  maxZoom: 15,
 }), L.tileLayer.wms("http://raster.nationalmap.gov/arcgis/services/Orthoimagery/USGS_EROS_Ortho_SCALE/ImageServer/WMSServer?", {
   minZoom: 16,
-  //maxZoom: 19,
+  maxZoom: 19,
   layers: "0",
   format: 'image/jpeg',
   transparent: true,
@@ -167,19 +167,43 @@ var highlightStyle = {
   radius: 10
 };
 
+//menampung variabel warna
+var ruangColors={
+  "Lab":"rgba(40,96,144,1.0)",
+  "Fasum":"rgba(234,137,150,1.0)",
+  "Admin" : "rgba(228,243,98,1.0)",
+  "Perpus" : "rgba(121,185,0,1.0)",
+  "Dosen" : "rgba(184,71,255,1.0)",
+  "Belajar" : "rgba(237,66,36,1.0)",
+
+};
+
+function style_ruang(feature) {
+  return {
+    opacity: 1,
+    color: 'rgba(0,0,0,0.1)',
+    dashArray: '',
+    lineCap: 'butt',
+    lineJoin: 'miter',
+    weight: 3.0, 
+    fillOpacity: 1,
+    fillColor: ruangColors[feature.properties['tipe']]
+  };
+}
 
 
 // GEDUNG DEKANAT FASTE
 
 var fst_lt1 = L.geoJson(null, {
-  style: function (feature) {
+  /*style: function (feature) {
     return {
       color: "green",
       //fill: false,
       opacity: 1,
       clickable: true
     };
-  },
+  }, */
+  style: style_ruang,
   onEachFeature: function (feature, layer) {
 	//untuk search, push masing-masing fitur ke array kita
     fst_lt1Search.push({
@@ -219,14 +243,15 @@ $.getJSON("data/fst_lt1.geojson", function (data) {
 
 
 var fst_lt2 = L.geoJson(null, {
-  style: function (feature) {
+  /*style: function (feature) {
     return {
       color: "black",
-      fill: false,
+      ////fill: false,
       opacity: 1,
       clickable: true
     };
-  },
+  }, */
+  style: style_ruang,
   onEachFeature: function (feature, layer) {
     fst_lt2Search.push({
       name: layer.feature.properties.nama,
@@ -267,14 +292,15 @@ $.getJSON("data/fst_lt2.geojson", function (data) {
 
 
 var fst_lt3 = L.geoJson(null, {
-  style: function (feature) {
+    /*style: function (feature) {
     return {
-      color: "black",
-      fill: false,
+      color: "green",
+      //fill: false,
       opacity: 1,
       clickable: true
     };
-  },
+  }, */
+  style: style_ruang,
   onEachFeature: function (feature, layer) {
     fst_lt3Search.push({
       name: layer.feature.properties.nama,
@@ -318,14 +344,15 @@ $.getJSON("data/fst_lt3.geojson", function (data) {
 // Gedung Baru
 
 var gb_lt1 = L.geoJson(null, {
-  style: function (feature) {
+    /*style: function (feature) {
     return {
-      color: "black",
-      fill: false,
+      color: "green",
+      //fill: false,
       opacity: 1,
       clickable: true
     };
-  },
+  }, */
+  style: style_ruang,
   onEachFeature: function (feature, layer) {
     gb_lt1Search.push({
       name: layer.feature.properties.nama,
@@ -367,14 +394,15 @@ $.getJSON("data/gb_lt1.geojson", function (data) {
 
 
 var gb_lt2 = L.geoJson(null, {
-  style: function (feature) {
+    /*style: function (feature) {
     return {
-      color: "black",
-      fill: false,
+      color: "green",
+      //fill: false,
       opacity: 1,
       clickable: true
     };
-  },
+  }, */
+  style: style_ruang,
   onEachFeature: function (feature, layer) {
     gb_lt2Search.push({
       name: layer.feature.properties.nama,
@@ -417,14 +445,15 @@ $.getJSON("data/gb_lt2.geojson", function (data) {
 
 
 var gb_lt3 = L.geoJson(null, {
-  style: function (feature) {
+    /*style: function (feature) {
     return {
-      color: "black",
-      fill: false,
+      color: "green",
+      //fill: false,
       opacity: 1,
       clickable: true
     };
-  },
+  }, */
+  style: style_ruang,
   onEachFeature: function (feature, layer) {
     gb_lt3Search.push({
       name: layer.feature.properties.nama,
@@ -471,14 +500,15 @@ $.getJSON("data/gb_lt3.geojson", function (data) {
 // kantin
 
 var kantin = L.geoJson(null, {
-  style: function (feature) {
+    /*style: function (feature) {
     return {
-      color: "blue",
+      color: "green",
       //fill: false,
       opacity: 1,
       clickable: true
     };
-  },
+  }, */
+  style: style_ruang,
   onEachFeature: function (feature, layer) {
     kantinSearch.push({
       name: layer.feature.properties.nama,
@@ -525,14 +555,15 @@ $.getJSON("data/kantin.geojson", function (data) {
 // lab_fst
 
 var lab_fst_lt1 = L.geoJson(null, {
-  style: function (feature) {
+  /* style: function (feature) {
     return {
       color: "black",
-      fill: false,
+      //fill: false,
       opacity: 1,
       clickable: true
     };
-  },
+  }, */
+  style: style_ruang,
   onEachFeature: function (feature, layer) {
     lab_fst_lt1Search.push({
       name: layer.feature.properties.nama,
@@ -576,14 +607,15 @@ $.getJSON("data/lab_fst_lt1.geojson", function (data) {
 
 
 var lab_fst_lt2 = L.geoJson(null, {
-  style: function (feature) {
+ /* style: function (feature) {
     return {
       color: "black",
       //fill: false,
       opacity: 1,
       clickable: true
     };
-  },
+  }, */
+  style: style_ruang,
   onEachFeature: function (feature, layer) {
     lab_fst_lt2Search.push({
       name: layer.feature.properties.nama,
@@ -629,14 +661,15 @@ $.getJSON("data/lab_fst_lt2.geojson", function (data) {
 
 // PSI
 var psi_lt1 = L.geoJson(null, {
-  style: function (feature) {
+    /*style: function (feature) {
     return {
-      color: "black",
+      color: "green",
       //fill: false,
       opacity: 1,
       clickable: true
     };
-  },
+  }, */
+  style: style_ruang,
   onEachFeature: function (feature, layer) {
     psi_lt1Search.push({
       name: layer.feature.properties.nama,
@@ -678,14 +711,15 @@ $.getJSON("data/psi_lt1.geojson", function (data) {
 
 
 var psi_lt2 = L.geoJson(null, {
-  style: function (feature) {
+    /*style: function (feature) {
     return {
-      color: "black",
-      fill: false,
+      color: "green",
+      //fill: false,
       opacity: 1,
       clickable: true
     };
-  },
+  }, */
+  style: style_ruang,
   onEachFeature: function (feature, layer) {
     psi_lt2Search.push({
       name: layer.feature.properties.nama,
@@ -810,8 +844,7 @@ var attributionControl = L.control({
 });
 attributionControl.onAdd = function (map) {
   var div = L.DomUtil.create("div", "leaflet-control-attribution");
-  div.innerHTML = "";
-  //"<span class='hidden-xs'>Developed by <a href='http://bryanmcbride.com'>bryanmcbride.com</a> | </span><a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
+  div.innerHTML = "<span class='hidden-xs'>Developed by <a href='http://sifsuska.github.io'>SIFsuska</a> | </span><a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
   return div;
 };
 map.addControl(attributionControl);
@@ -869,7 +902,7 @@ var baseLayers = {
 
 var groupedOverlays = {
   "Dekanat": {
-    "<img src='assets/img/faste.png' width='18' height='18'>&nbsp;Lantai 1": fst_lt1,
+    "Lantai 1": fst_lt1,
     "Lantai 2": fst_lt2,
     "Lantai 3": fst_lt3 
   },
@@ -954,7 +987,7 @@ $(document).one("ajaxStop", function () {
       return Bloodhound.tokenizers.whitespace(d.name);
     },
     queryTokenizer: Bloodhound.tokenizers.whitespace,
-    local: fst_lt1Search,
+    local: fst_lt3Search,
     limit: 10
   });
   fst_lt3BH.initialize();  
@@ -1032,7 +1065,17 @@ $(document).one("ajaxStop", function () {
   });
   kantinBH.initialize();
  
-
+  //lab
+  var lab_fst_lt1BH = new Bloodhound({
+    name: "Lab Lantai 1",
+    datumTokenizer: function (d) {
+      return Bloodhound.tokenizers.whitespace(d.name);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: lab_fst_lt1Search,
+    limit: 10
+  });
+  lab_fst_lt1BH.initialize();
 	
   /*
   var geonamesBH = new Bloodhound({
@@ -1135,6 +1178,13 @@ $(document).one("ajaxStop", function () {
     templates: {
       header: "<h4 class='typeahead-header'>Kantin</h4>"
     }
+  }, {
+    name: "lab_fst_lt1",
+    displayKey: "name",
+    source: lab_fst_lt1BH.ttAdapter(),
+    templates: {
+      header: "<h4 class='typeahead-header'>Lab Lantai 1</h4>"
+    }  
   }).on("typeahead:selected", function (obj, datum) {
     //if (datum.source === "Fst") {
       map.fitBounds(datum.bounds);
